53be36d8c7cb373164e813b1eb1821258d740f37
shubham Rawat <rawatshubam269@gmail.com>
2024-01-25 17:19:37 +0530

    useEffect dep fix

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ function App() {
 
   useEffect(() => {
     getCountryData(`https://crio-location-selector.onrender.com/countries`);
-  }, [country]);
+  }, []);
 
   return (
     <div className="main__div">
